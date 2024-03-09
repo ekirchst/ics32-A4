689f77990fa660c2b908192971734938912ce477
ekirchst <ekirchst@uci.edu>
2024-03-08 16:07:18 -0800

    part.1 done

---
+++ LastFM.py
@@ -0,0 +1,15 @@
+# Evan
+# ekirchst@uci.edu
+# 59946460
+from urllib import request
+import json as js
+from datetime import datetime
+
+class LastFM:
+
+    def __init__(self) -> None:
+        
+    def set_apikey(self, apikey:str) -> None:
+        self.apikey = apikey
+    
+    
--- OpenWeather.py
+++ OpenWeather.py
@@ -24,15 +24,25 @@ class OpenWeather:
 
     
     def load_data(self) -> None:
-        temp = f"{self.url}/weather?zip={self.zip_code},{self.country_code}&appid={self.apikey}"
-        response = request.urlopen(temp)
-        re = js.loads(response.read())
-        self.temperature = re['main']['temp']
-        self.high_temperature = re['main']['temp_max']
-        self.low_temperature = re['main']['temp_min']
-        self.longitude = re['coord']['lon']
-        self.latitude = re['coord']['lat']
-        self.description = re['weather'][0]['description']
-        self.humidity = re['main']['humidity']
-        self.sunset = datetime.utcfromtimestamp(re['sys']['sunset']).strftime('%Y-%m-%d %H:%M:%S')
-        self.city = re['name']
+        try:
+            temp = f"{self.url}/weather?zip={self.zip_code},{self.country_code}&appid={self.apikey}"
+            response = request.urlopen(temp)
+            re = js.loads(response.read())
+            if response.getcode() != 404 or 503:
+                self.temperature = re['main']['temp']
+                self.high_temperature = re['main']['temp_max']
+                self.low_temperature = re['main']['temp_min']
+                self.longitude = re['coord']['lon']
+                self.latitude = re['coord']['lat']
+                self.description = re['weather'][0]['description']
+                self.humidity = re['main']['humidity']
+                self.sunset = datetime.utcfromtimestamp(re['sys']['sunset']).strftime('%Y-%m-%d %H:%M:%S')
+                self.city = re['name']
+            elif response.getcode() == 404:
+                print("404 ERROR, API UNAVAILABLE")
+            elif response.getcode() == 503:
+                print("503 ERROR")
+        except request.URLError as e:
+            print(f"ERROR {e}")
+        except js.JSONDecodeError as e:
+            print(f"ERROR {e}")
